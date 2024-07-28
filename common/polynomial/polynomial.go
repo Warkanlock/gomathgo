619457633f42c3592a4d2bdd320e2d435f4ef41d
Warkanlock <warkanlock@gmail.com>
2024-07-28 12:58:56 +0200

    chore(common): alter `Degree` method to return correct values

--- common/polynomial/polynomial.go
+++ common/polynomial/polynomial.go
@@ -19,13 +19,49 @@ type Polynomial struct {
 }
 
 // Create a polynomial based on a list of coefficients
-func (Polynomial) New(coefficients []Coefficient) *Polynomial {
+func NewPolynomial(coefficients []Coefficient) *Polynomial {
 	return &Polynomial{
 		coefficients,
 	}
 }
 
-func (Polynomial) Interpolate(points []Point) (*Polynomial, error) {
+// Add two polynomials together
+func (p Polynomial) Add(p2 Polynomial) *Polynomial {
+	// pick the maximum degree of the two polynomials
+	newCoefficients := make([]Coefficient, max(len(p.coefficients), len(p2.coefficients)))
+
+	// coeffients of the first polynomial
+	for i, c := range p2.coefficients {
+		newCoefficients[i] += c
+
+		// if there's a coefficient in the base polynomial
+		if len(p.coefficients) > 0 && i < len(p.coefficients) {
+			newCoefficients[i] += p.coefficients[i]
+		}
+	}
+
+	return &Polynomial{
+		coefficients: newCoefficients,
+	}
+}
+
+// Multiply two polynomials together
+func (p Polynomial) Multiply(p2 Polynomial) *Polynomial {
+	coefficients := make([]Coefficient, len(p.coefficients)+len(p2.coefficients)-1)
+
+	for i, c1 := range p.coefficients {
+		for j, c2 := range p2.coefficients {
+			coefficients[i+j] += c1 * c2
+		}
+	}
+
+	return &Polynomial{
+		coefficients,
+	}
+}
+
+// Interpolate a polynomial based on a list of points
+func Interpolate(points []Point) (*Polynomial, error) {
 	if len(points) == 0 {
 		return nil, fmt.Errorf("cannot perform an interpolation without any valid points")
 	}
@@ -46,19 +82,62 @@ func (Polynomial) Interpolate(points []Point) (*Polynomial, error) {
 		return nil, fmt.Errorf("cannot perform an interpolation with duplicate (x,y) pair values")
 	}
 
-	return nil, nil
+	var terms []Polynomial
+
+	// iterate over the points
+	for i, pointA := range points {
+		term := NewPolynomial([]Coefficient{1})
+		// evaluate polynomial at (x,y) on point
+		x, y := pointA.X, pointA.Y
+
+		for j, pointB := range points {
+			if i == j {
+				continue
+			}
+
+			xj := pointB.X
+
+			a0 := Coefficient(-xj / (x - xj))
+			a1 := Coefficient(1 / (x - xj))
+
+			// create a polynomial based on the x values
+			p := NewPolynomial([]Coefficient{a0, a1})
+
+			// multiply the polynomial with the term
+			term = term.Multiply(*p)
+		}
+
+		// multiply the term with the y value
+		term = term.Multiply(*NewPolynomial([]Coefficient{Coefficient(y)}))
+
+		// append term to a list of terms
+		terms = append(terms, *term)
+	}
+
+	if terms == nil {
+		return nil, fmt.Errorf("cannot perform an interpolation without any valid terms")
+	}
+
+	// add the terms together
+	base := NewPolynomial([]Coefficient{0})
+
+	for _, term := range terms {
+		base = base.Add(term)
+	}
+
+	return base, nil
 }
 
 // Retrieve the degree of the polynomial
 func (p Polynomial) Degree() int {
-	return len(p.coefficients)
+	return len(p.coefficients) - 1
 }
 
 // Show a polynomial based on the points provided
 func (p Polynomial) String() string {
 	var completePolynomial string
 
-	completePolynomial = "\nf(x) = "
+	completePolynomial = "f(x) = "
 
 	for idx, coefficient := range p.coefficients {
 		if idx == 0 {
@@ -74,7 +153,7 @@ func (p Polynomial) String() string {
 		completePolynomial = fmt.Sprintf("%s + %.2fx^%d", completePolynomial, coefficient, idx)
 	}
 
-	completePolynomial = fmt.Sprintf("%s\n", completePolynomial)
+	completePolynomial = fmt.Sprintf("%s", completePolynomial)
 
 	return fmt.Sprintf(completePolynomial)
 }
--- common/polynomial/polynomial_test.go
+++ common/polynomial/polynomial_test.go
@@ -7,7 +7,7 @@ import (
 func TestNewPolynomial(t *testing.T) {
 	coefficients := []Coefficient{1, 2, 3}
 
-	polynomial := Polynomial{}.New(coefficients)
+	polynomial := NewPolynomial(coefficients)
 
 	if len(polynomial.coefficients) != 3 {
 		t.Errorf("New(%v) = %v; want %v", coefficients, polynomial.coefficients, coefficients)
@@ -23,11 +23,34 @@ func TestNewPolynomial(t *testing.T) {
 func TestGetDegree(t *testing.T) {
 	coefficients := []Coefficient{1, 2, 3}
 
-	polynomial := Polynomial{}.New(coefficients)
+	polynomial := NewPolynomial(coefficients)
 
 	degree := polynomial.Degree()
 
 	if degree != 3 {
 		t.Errorf("Degree() = %d; want %d", degree, 3)
+	}
+}
+
+func TestInterpolate(t *testing.T) {
+	points := []Point{
+		{1, 2},
+		{2, 3},
+	}
+
+	polynomial, err := Interpolate(points)
+
+	if err != nil {
+		t.Errorf("Interpolate(%v) = %v; want %v", points, err, nil)
+	}
+
+	if polynomial.Degree() != 1 {
+		t.Errorf("Interpolate(%v) = %d; want %d", points, polynomial.Degree(), 1)
+	}
+
+	structure := polynomial.String()
+
+	if structure != "f(x) = 1.00 + 1.00x^1" {
+		t.Errorf("Interpolate(%v) = %s; want %s", points, structure, "f(x) = 1.00 + 1.00x^1")
 	}
 }
