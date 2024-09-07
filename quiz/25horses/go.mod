8adb4510ae33c275ba6bfd4f91afa27d98e95b51
Warkanlock <warkanlock@gmail.com>
2024-09-07 08:29:04 +0200

    chore(refactor): refactor has been put in place at 2024-09-07T08:29:04+0200

---
+++ quiz/25horses/go.mod
@@ -0,0 +1,3 @@
+module gomathgo/quiz/25horses
+
+go 1.22.3
---
+++ quiz/25horses/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+// technically a race it's just a sort from lowest to highest time
+func race(horses []int) []int {
+	sort.Slice(horses, func(i, j int) bool {
+		return horses[i] > horses[j]
+	})
+
+	return horses
+}
+
+func assign_horses(horses []int, size uint) {
+	for i := 1; i <= int(size); i++ {
+		// to avoid starting it in zero
+		horses[i-1] = i
+	}
+}
+
+func main() {
+	horses_size := uint(25)
+
+	// slices are already by reference type
+	horses := make([]int, horses_size)
+
+	// assign to each horse an specific id
+	assign_horses(horses, horses_size)
+
+	fmt.Printf("All horses: %d\n", horses)
+}
